43ce1e754c99f7754532b08e823f8651aafb57d5
KaloyanPurvi <kaloyanivanov618@gmail.com>
2024-02-28 19:11:46 +0200

    Iterative method

--- Week 1/fibRec.hs
+++ Week 1/fibRec.hs
@@ -5,8 +5,21 @@
 -}
 main::IO()
 main=do
- print (fibRec 4)
-fibRec::Int->Int
-fibRec 0 = 1
-fibRec 1 = 1
-fibRec x=fibRec(x-1)+fibRec(x-2)
+  print (fibRecIter 4)
+
+fibRecIter::Int->Int
+fibRecIter x=
+    fibHelper 0 1 x
+
+fibHelper::Int->Int->Int->Int
+fibHelper a b 0= (b)
+fibHelper a b x=
+    fibHelper b (a+b) (x-1)
+
+
+
+fibRecNotIter::Int->Int
+fibRecNotIter 0 = 1
+fibRecNotIter 1 = 1
+fibRecNotIter x=fibRecNotIter(x-1)+fibRecNotIter(x-2)
+
