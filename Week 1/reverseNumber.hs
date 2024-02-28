f9e6044c51fc9df0a850f1d7815c9bc22efee186
KaloyanPurvi <kaloyanivanov618@gmail.com>
2024-02-28 19:51:59 +0200

    wrote the code for reverseNumber

---
+++ Week 1/reverseNumber.hs
@@ -0,0 +1,16 @@
+--CUSTOM TASK that I wanted to try
+
+main::IO()
+main=do
+    print (reverseNumberRecNotIter 102)
+
+
+reverseNumber::Int->Int
+reverseNumber start=
+    reverseNumberHelp 0 start
+
+reverseNumberHelp::Int->Int->Int
+reverseNumberHelp result 0 = result
+reverseNumberHelp result start=
+    reverseNumberHelp ((result*10)+(mod start 10)) (div start 10)
+
