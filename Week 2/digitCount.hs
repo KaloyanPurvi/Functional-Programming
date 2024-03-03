052f55e6e85e62f36309a1c4b71a0fcb202bf882
KaloyanPurvi <kaloyanivanov618@gmail.com>
2024-03-03 14:07:14 +0200

    new recursive version

--- Week 2/digitCount.hs
+++ Week 2/digitCount.hs
@@ -1,3 +1,11 @@
+main::IO()
+main = do
+    print (myDigitCount 1233)
+    print(myDigitCountRec 1233) 
+    print (myDigitCount 0)
+    print(myDigitCountRec 0)
+
+{-ITERATIVE-}
 myDigitCount::Int->Int
 myDigitCount 0 = 1
 myDigitCount a = myDigitCountHelper a 0
@@ -5,3 +13,9 @@ myDigitCount a = myDigitCountHelper a 0
         myDigitCountHelper::Int->Int->Int
         myDigitCountHelper 0 count = count
         myDigitCountHelper a count=myDigitCountHelper ( a `div` 10) (count+1)
+
+{-BASIC-}
+myDigitCountRec::Int->Int
+myDigitCountRec x
+ | x<10 = 1
+ | otherwise = 1 + myDigitCountRec(div x 10)
